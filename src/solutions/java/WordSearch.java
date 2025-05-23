5f4985115ccf21fe2aced219b35fedb066bd7bce
jonasweet9900 <jonasweet9900@gmail.com>
2025-05-23 16:59:20 +0900

    [Python, Java] Updated '79. Word Search'

--- src/solutions/java/WordSearch.java
+++ src/solutions/java/WordSearch.java
@@ -5,7 +5,7 @@ package solutions.java;
 	- `Link`: https://leetcode.com/problems/word-search/
 # Solution
 	- `Author`: Kyungtaek Lim (Jonas)
-	- `Date`: Apr 15, 2025
+	- `Date`: May 23, 2025
 	- `Answer`: exist
  */
 public class WordSearch {
@@ -14,48 +14,35 @@ public class WordSearch {
     # Option #1
     - O (m * n * 3^L) (m = row, n = col, L = the length of word)
 	 */
-    public static boolean exist(char[][] board, String word) {
-        
-    	int m = board.length;
-    	int n = board[0].length; 
-    	
-    	char startChar = word.charAt(0);
-    	for (int i = 0; i < m; i++) {
-    		for (int j = 0; j < n; j++) {
-    			if (board[i][j] == startChar) {
-    				if (search(board, word, m, n, i, j)) return true;
-    			}
-    		}
-    	}
-    	return false;
+    public boolean exist(char[][] board, String word) {
+
+        int m = board.length, n = board[0].length;
+        char[] words = word.toCharArray();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, words, 0, i, j)) return true;
+            }
+        }
+        return false;
     }
-    
+
     // Move and search for a route (Recursive)
-    public static boolean search(char[][] board, String word, int m, int n, int x, int y) {
-    	
-    	// Success
-    	if (word.isEmpty()) return true;
-    	
-    	// Edge Case
-    	if (x < 0 || y < 0 || x >= m || y >= n) return false;
-    	if (board[x][y] == '0') return false;
-    	
-    	// Matches
-    	char startChar = word.charAt(0);
-    	if (board[x][y] != startChar) return false;
-    	word = word.substring(1); 
-    	
-    	// Mark
-    	board[x][y] = '0';
-    	
-    	// Move
-    	if (search(board, word, m, n, x + 1, y)) return true;
-    	if (search(board, word, m, n, x - 1, y)) return true;
-    	if (search(board, word, m, n, x, y + 1)) return true;
-    	if (search(board, word, m, n, x, y - 1)) return true;
-    	
-    	// Unmark
-    	board[x][y] = startChar;
-    	return false;
+    private boolean dfs(char[][] board, char[] word, int index, int x, int y) {
+
+        if (index == word.length) return true;
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
+        if (board[x][y] != word[index]) return false;
+
+        char temp = board[x][y];
+        board[x][y] = '0';
+
+        if (dfs(board, word, index + 1, x + 1, y) ||
+            dfs(board, word, index + 1, x, y + 1) ||
+            dfs(board, word, index + 1, x - 1, y) ||
+            dfs(board, word, index + 1, x, y - 1))
+            return true;
+
+        board[x][y] = temp;
+        return false;
     }
 }
--- src/solutions/python/WordSearch.py
+++ src/solutions/python/WordSearch.py
@@ -5,7 +5,7 @@ from typing import List
 	- `Link`: https://leetcode.com/problems/word-search/
 # Solution
 	- `Author`: Kyungtaek Lim (Jonas)
-	- `Date`: Apr 15, 2025
+	- `Date`: May 23, 2025
 	- `Answer`: exist
 '''
 class Solution:
@@ -17,46 +17,34 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
         m, n = len(board), len(board[0])
+        len_word = len(word)
 
-        # Move and search for a route (Recursive)
-        def search(board_: List[List[str]], word_: str, x: int, y: int) -> bool:
-            
-            # Success
-            if not word_:
+        def dfs(x: int, y: int, index: int) -> bool:
+            if index == len_word:
                 return True
-            
-            # Edge Case
-            if not (0 <= x < m and 0 <= y < n):
+            if x < 0 or y < 0 or x >= m or y >= n:
                 return False
-            
-            # Matches
-            startChar_ = word_[0]
-            if startChar_ != board_[x][y]:
+            if board[x][y] != word[index]:
                 return False
-            word_ = word_[1:]
 
-            # Mark
-            board_[x][y] = "0"
+            temp = board[x][y]
+            board[x][y] = '0'
 
-            # Move
-            if search(board_, word_, x + 1, y):
-                return True
-            if search(board_, word_, x - 1, y):
-                return True
-            if search(board_, word_, x, y + 1):
-                return True
-            if search(board_, word_, x, y - 1):
+            if (dfs(x + 1, y, index + 1)
+                or dfs(x - 1, y, index + 1)
+                or dfs(x, y + 1, index + 1)
+                or dfs(x, y - 1, index + 1)
+                ):
                 return True
 
-            # Unmark
-            board_[x][y] = startChar_
+            board[x][y] = temp
+
             return False
 
-        startChar = word[0]
+
         for i in range(m):
             for j in range(n):
-                if board[i][j] == startChar:
-                    if search(board, word, i, j):
-                        return True
-                    
+                if dfs(i, j, 0):
+                    return True
+
         return False
