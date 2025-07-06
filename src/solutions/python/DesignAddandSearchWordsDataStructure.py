fb8a2d97e2f9c3a7b77ec90782c98b0773b0105a
jonasweet9900 <jonasweet9900@gmail.com>
2025-07-06 17:14:27 +0900

    [Python] Updated '211. Design Add and Search Words Data Structure'

--- src/solutions/python/DesignAddandSearchWordsDataStructure.py
+++ src/solutions/python/DesignAddandSearchWordsDataStructure.py
@@ -7,7 +7,7 @@ import collections
 # Solution
 	- `Author`: Kyungtaek Lim (Jonas)
 	- `Date`: July 6, 2025
-	- `Answer`: WordDictionary
+	- `Answer`: WordDictionary / WordDictionaryPrefixTree
 '''
 
 '''
@@ -35,9 +35,66 @@ class WordDictionary:
     def search(self, word: str) -> bool:
         return word in self.my_set
 
+'''
+# Option #2
+- Prefix Tree
+- O(L), O(26^L) (L = The length of the word)
+'''
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class WordDictionaryPrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+
+    # O(L) (L = The length of the word)
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TreeNode()
+            curr = curr.children[c]
+        curr.end = True
+
+    # O(26^L) (L = The length of the word)
+    def search(self, word: str) -> bool:
+        n = len(word)
+
+        def dfs(curr: TreeNode, i: int) -> bool:
+            if i >= n:
+                return curr.end
+            
+            if word[i] == '.':
+                for temp in curr.children.values():
+                    if dfs(temp, i + 1):
+                        return True
+                return False
+            elif word[i] not in curr.children:
+                return False
+
+            return dfs(curr.children[word[i]], i + 1)
+
+        return dfs(self.root, 0)
+    
+
+
 
 if __name__ == '__main__':
     wordDictionary = WordDictionary()
+    wordDictionary.addWord("bad")
+    wordDictionary.addWord("dad")
+    wordDictionary.addWord("mad")
+    
+    print(wordDictionary.search("pad"))
+    print(wordDictionary.search("bad"))
+    print(wordDictionary.search(".ad"))
+    print(wordDictionary.search("b.."))
+
+
+    wordDictionary = WordDictionaryPrefixTree()
     wordDictionary.addWord("bad")
     wordDictionary.addWord("dad")
     wordDictionary.addWord("mad")
