cb2759895f3379122c3bf59045c752403e66d099
jonasweet9900 <jonasweet9900@gmail.com>
2025-07-06 16:48:35 +0900

    [Python] Updated '208. Implement Trie (Prefix Tree)'

--- src/solutions/python/ImplementTriePrefixTree.py
+++ src/solutions/python/ImplementTriePrefixTree.py
@@ -37,6 +37,53 @@ class Trie:
     def startsWith(self, prefix: str) -> bool:
         return self.my_dict.get(prefix, False)
 
+
+
+
+'''
+# Option #2
+- Trie (Prefix Tree)
+- O(L), O(L), O(P) (L = The length of the words, P = the length of the prefixes)
+- ref) https://www.youtube.com/watch?v=oobqoCJlHA0
+'''
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False # You cannot tell the end of the tree with only children cause nodes are shared for other words
+
+class TriePrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+
+    # O(L) (L = The length of the words)
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TreeNode()
+            curr = curr.children[c]
+        curr.end = True
+
+    # O(L) (L = The length of the words)
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.end
+
+    # O(P) (P = The length of the prefixes)
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+
+
     
 if __name__ == '__main__':
     word = "1234"
@@ -45,3 +92,11 @@ if __name__ == '__main__':
     obj.insert(word)
     param_2 = obj.search(word)
     param_3 = obj.startsWith(prefix)
+    
+    word = "1234"
+    prefix = "12"
+    obj = TriePrefixTree()
+    obj.insert(word)
+    param_2 = obj.search(word)
+    param_3 = obj.startsWith(prefix)
+    print(param_2, param_3)
