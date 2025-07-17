20f1c21f667dd401d2fa1e0ccb8bc2f22bcd0738
jonasweet9900 <jonasweet9900@gmail.com>
2025-07-18 03:29:36 +0900

    [TypeScript] Added '572. Subtree of Another Tree'

---
+++ src/solutions/typescript/SubtreeofAnotherTree.ts
@@ -0,0 +1,84 @@
+
+/*
+# Problem
+ 	- `Link`: https://leetcode.com/problems/subtree-of-another-tree/
+# Solution
+	- `Author`: Kyungtaek Lim (Jonas)
+	- `Date`: July 17, 2025
+	- `Answer`: isSubtreeDfsRecursive / isSubtreeDfsRecursiveAdvanced
+*/
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+
+/*
+# Option #1
+- DFS Recursive
+- With only one function
+- O(m * n) (m = the number of root nodes, n = the number of subRoot nodes)
+*/
+function isSubtreeDfsRecursive(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+
+    let isOn: boolean = false;
+
+    function dfs(node: TreeNode | null, subNode: TreeNode | null): boolean {
+
+        if (node === null && subNode === null) return true;
+        if (node === null || subNode === null) return false;
+        if (isOn && node.val !== subNode.val) return false;
+
+        if (node.val === subNode.val) {
+            const wasOn = isOn;
+            isOn = true;
+            if (dfs(node.left, subNode.left) && dfs(node.right, subNode.right)) {
+                return true;
+            } else if (wasOn) {
+                return false;
+            } else {
+                isOn = false;
+            }
+        }
+
+        return dfs(node.left, subNode) || dfs(node.right, subNode);
+    }
+
+    return dfs(root, subRoot);
+};
+
+
+
+
+/*
+# Option #2
+- DFS Recursive (Advanced)
+- With two function
+- O(m * n) (m = the number of root nodes, n = the number of subRoot nodes)
+*/
+function isSubtreeDfsRecursiveAdvanced(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+
+    function isSame(n1: TreeNode | null, n2: TreeNode | null): boolean {
+        if (n1 === null && n2 === null) return true;
+        if (n1 === null || n2 === null) return false;
+        if (n1.val !== n2.val) return false;
+        return isSame(n1.left, n2.left) && isSame(n1.right, n2.right);
+    }
+
+    function dfs(n1: TreeNode | null): boolean {
+        if (isSame(n1, subRoot)) return true;
+        if (n1 === null) return false;
+
+        return dfs(n1!.left) || dfs(n1!.right);
+    }
+
+    return dfs(root);
+}
