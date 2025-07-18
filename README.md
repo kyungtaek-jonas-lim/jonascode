8e7df0cc1bf494005b09beb3b17352a9f47a7a85
jonasweet9900 <jonasweet9900@gmail.com>
2025-07-18 22:42:37 +0900

    [TypeScript] Added '235. Lowest Common Ancestor of a Binary Search Tree'

--- README.md
+++ README.md
@@ -221,7 +221,7 @@
 	- Solution ([Java](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/java/KthSmallestElementinaBST.java), [Python](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/python/KthSmallestElementinaBST.py), [TypeScript](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/typescript/KthSmallestElementinaBST.ts))
 66. Lowest Common Ancestor of a Binary Search Tree (235) (*)
 	- [Problem](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
-	- Solution ([Java](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/java/LowestCommonAncestorofaBinarySearchTree.java), [Python](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/python/LowestCommonAncestorofaBinarySearchTree.py))
+	- Solution ([Java](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/java/LowestCommonAncestorofaBinarySearchTree.java), [Python](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/python/LowestCommonAncestorofaBinarySearchTree.py), [TypeScript](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/typescript/LowestCommonAncestorofaBinarySearchTree.ts))
 67. Implement Trie (Prefix Tree) (208)
 	- [Problem](https://leetcode.com/problems/implement-trie-prefix-tree/)
 	- Solution ([Java](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/java/ImplementTriePrefixTree.java), [Python](https://github.com/kyungtaek-jonas-lim/jonascode/blob/main/src/solutions/python/ImplementTriePrefixTree.py))
---
+++ src/solutions/typescript/LowestCommonAncestorofaBinarySearchTree.ts
@@ -0,0 +1,101 @@
+
+/*
+# Problem
+ 	- `Link`: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# Solution
+	- `Author`: Kyungtaek Lim (Jonas)
+	- `Date`: July 18, 2025
+	- `Answer`: lowestCommonAncestorDfsRecursiveAdvanced / lowestCommonAncestorDfsRecursive / lowestCommonAncestorSearch
+*/
+
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+/*
+# Option #1
+- DFS Recursive
+- O(log N) ~ O(N)
+*/
+function lowestCommonAncestorDfsRecursive(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	
+    if (root === null) return null;
+    if (p === null || q === null) return null; // For IDE
+
+    if ((root.val >= p.val && root.val < q.val) || (root.val > p.val && root.val <= q.val)
+        || (root.val <= p.val && root.val > q.val) || (root.val < p.val && root.val >= q.val)) return root;
+
+    let result: TreeNode | null = null;
+    if (root.val > p.val && root.val > q.val) {
+        result = lowestCommonAncestorDfsRecursive(root.left, p, q);
+    } else {
+        result = lowestCommonAncestorDfsRecursive(root.right, p, q);
+    }
+
+    return result === null ? root : result;
+};
+
+
+
+/*
+# Option #2
+- DFS Recursive Advanced
+- O(log N) ~ O(N)
+*/
+function lowestCommonAncestorDfsRecursiveAdvanced(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	
+    let result: TreeNode | null = null;
+
+    function dfs(node: TreeNode | null): void {
+        if (node === null) return;
+        if (p === null || q === null) return; // For IDE
+        if (node.val > p.val && node.val > q.val) {
+            result = node;
+            dfs(node.left);
+        } else if (node.val < p.val && node.val < q.val) {
+            result = node;
+            dfs(node.right);
+        } else {
+            result = node;
+        }
+    }
+
+    dfs(root);
+    return result;
+};
+
+
+
+/*
+# Option #3
+- Search
+- O(log N) ~ O(N)
+*/
+function lowestCommonAncestorSearch(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	
+    let result: TreeNode | null = null;
+
+    while (root !== null) {
+        if (p === null || q === null) continue; // For IDE
+        if (root.val < p.val && root.val < q.val) {
+            result = root;
+            root = root.right;
+        } else if (root.val > p.val && root.val > q.val) {
+            result = root;
+            root = root.left;
+        } else {
+            result = root;
+            break;
+        }
+    }
+    
+    return result;
+};
